eaf805ec80a2942501e238fd00433cd963f9eca4
Thiago Alessandro Batista <tbatista@weg.net>
2024-03-19 15:59:34 -0300

    feat: set up dependencies and aplication properties

---
+++ src/main/java/com/listedin/listedinapi/ListedinApiApplication.java
@@ -0,0 +1,13 @@
+package com.listedin.listedinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListedinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListedinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listedin/listedinapi/ListedinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listedin.listedinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListedinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
